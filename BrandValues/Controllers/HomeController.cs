1e8a12e6d6a53657a0d584ad3f6a8a9965c59932
Dave Maxwell <maxweld@tcd.ie>
2014-07-09 16:48:04 +0100

    upload to S3 working

--- BrandValues/Controllers/HomeController.cs
+++ BrandValues/Controllers/HomeController.cs
@@ -1,4 +1,15 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.Drawing.Imaging;
+using Amazon;
+using System.IO;
+using System.Drawing;
+using System.Collections.Specialized;
+using System.Configuration;
+
 
 namespace BrandValues.Controllers {
 
@@ -16,5 +27,60 @@ namespace BrandValues.Controllers {
         {
             return View("Upload");
         }
+
+        public class ViewDataUploadFilesResult
+        {
+            public string Name { get; set; }
+            public int Length { get; set; }
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(HttpPostedFileBase[] files)
+        {
+            var myResponse = "";
+
+            foreach (HttpPostedFileBase file in files)
+            {
+
+                if (file.ContentLength > 0)
+                {
+                   NameValueCollection appConfig = ConfigurationManager.AppSettings;
+                   string accessKey = appConfig["S3AWSAccessKey"];
+                   string secretKey = appConfig["S3AWSSecretKey"];
+
+
+                   IAmazonS3 client;
+
+                   var filePath = "video/" + file.FileName;
+
+                    try
+                    {
+                        using (client = AWSClientFactory.CreateAmazonS3Client(accessKey, secretKey))
+                        {
+                            PutObjectRequest request = new PutObjectRequest();
+
+                            request.BucketName = "valuescompetition-useruploads";
+                            request.CannedACL = S3CannedACL.Private;
+                            request.ServerSideEncryptionMethod = ServerSideEncryptionMethod.AES256;
+                            request.Key = filePath;
+                            request.InputStream = file.InputStream;
+
+                            PutObjectResponse response = client.PutObject(request);
+                            myResponse = response.HttpStatusCode.ToString();
+                        }
+                    }
+                    catch (AmazonS3Exception s3Exception)
+                    {
+                        //s3Exception.InnerException
+                        ViewBag.Message = s3Exception.Message;
+                        return View("Upload");
+                    }
+                }
+            }
+
+            ViewBag.Message = myResponse;
+            return View("Upload");
+        }
+
     }
 }
