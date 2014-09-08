a5d936fc35b06c652151514c00a0a218c5e9d0f9
Dave Maxwell <maxweld@tcd.ie>
2014-09-08 11:11:36 +0100

    Fixed FAQs

---
+++ BrandValues/Models/Poll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BrandValues.Models
+{
+    public class Poll
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> Completed = new List<string>();
+
+        public Poll()
+        {
+        }
+    }
+}
